b453ff6ead9c02df18071770119635bfcfa599c2
d.ivanov <cool-di@mail.ru>
2019-04-08 19:59:30 +0300

    Забытое меню

---
+++ .top.menu.php
@@ -0,0 +1,39 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Обучение", 
+		"/course/", 
+		Array(), 
+		Array("class"=>"study"), 
+		"" 
+	),
+	Array(
+		"Материалы", 
+		"/materials/", 
+		Array(), 
+		Array("class"=>"materials"),  
+		"" 
+	),
+	Array(
+		"Вебинары", 
+		"/webinars/", 
+		Array(), 
+		Array("class"=>"webinars"), 
+		"" 
+	),
+	Array(
+		"Конкурсы", 
+		"/tenders/", 
+		Array(), 
+		Array("class"=>"tenders"), 
+		"" 
+	),
+	Array(
+		"Контакты", 
+		"/contacts/", 
+		Array(), 
+		Array("class"=>"contacts"),  
+		"" 
+	)
+);
+?>
