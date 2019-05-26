74e92b6fe8344b425c37dd18820dc5e42a7eaff9
d.ivanov <cool-di@mail.ru>
2019-05-26 14:51:27 +0300

    фикс

--- local/php_interface/agent.php
+++ local/php_interface/agent.php
@@ -4,7 +4,6 @@ use IT\Intranet\Exchange\Application\ProductBonusCsv;
 
 function loadProductsBonus()
 {
-    mail('cool-di@mail.ru', 'Агент', 'Агент битрикс');
     $productBonus = new ProductBonusCsv();
     $productBonus->updateBonus();
     return "loadProductsBonus();";
